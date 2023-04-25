571f709943884c89e65e3f684a21cb7b99e239d3
1chello <underzero@bk.ru>
2023-04-25 08:09:25 +0300

    Fix

--- JavaEdu/src/main/java/Seminar4/Task1/Main.java
+++ JavaEdu/src/main/java/Seminar4/Task1/Main.java
@@ -1,15 +1,17 @@
 package Seminar4.Task1;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Main {
-    public ListNode reverseList(ListNode head){
-        ListNode x = null;
-        ListNode current = head;
-        while (current != null){
-            ListNode temp = current.next;
-            current.next = x;
-            x = current;
-            current = temp;
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(1,25,45,65,32,454,575,242));
+        Integer sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i) %2 == 0){
+                sum += list.get(i);
+            }
         }
-        return  x;
+        System.out.println(sum);
     }
 }
